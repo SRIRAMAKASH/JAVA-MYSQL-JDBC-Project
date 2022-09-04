9640736ed87c4499528c8f857d099ea1b0c8a4e3
Sriramakash Palanivelu <sriramakash222@gmail.com>
2022-09-04 22:37:11 +0530

    AdminScreen of Application is Created

---
+++ src/com/ecom/UI/AdminScreen.java
@@ -0,0 +1,71 @@
+package com.ecom.UI;
+
+import java.util.Scanner;
+
+public class AdminScreen 
+{
+	Scanner sc =new Scanner(System.in);
+	String email,password;
+	boolean status=true;
+	int ch=0;
+	
+	public void login()
+	{
+		System.out.println("====== Admin Login ======");
+		System.out.println("Enter the email :");
+		email=sc.next();
+		System.out.println("Enter the Password :");
+		password=sc.next();
+		
+		// validate with DB
+		if(status==true)
+		{
+			product();
+		}
+		else
+		{
+			System.out.println("Email and password are Incorrect");
+		}
+	}
+	
+	public void product()
+	{
+		do 
+		{
+		System.out.println("====== Welcome to Product Of Admin ======");
+		System.out.println("1.Insert the product");
+		System.out.println("2.Select the Product");
+		System.out.println("3.Search the Product By Id");
+		System.out.println("4.Update the Product");
+		System.out.println("5.Delete the Product");
+		System.out.println("0.Exit");
+		ch = sc.nextInt();
+		
+
+		switch (ch) {
+		case 1:
+			System.out.println("Insert the product");
+			break;
+		case 2:
+			System.out.println("Select the Product ");
+			break;
+		case 3:
+			System.out.println("Search the Product By Id");
+			break;
+		case 4:
+			System.out.println("Update the Product");
+			break;
+		case 5:
+			System.out.println("Delete the Product");
+			break;
+		case 0:
+			System.out.println(" Thank You...");
+			break;
+		default:
+			System.out.println("Invalide Choice");
+			break;
+		}
+		}while(ch!=0);
+	}
+
+}
--- src/com/ecom/UI/HomeScreen.java
+++ src/com/ecom/UI/HomeScreen.java
@@ -7,18 +7,18 @@ public class HomeScreen {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int ch = 0;
-		
+		AdminScreen admin=new AdminScreen();
 		do
 		{
 		System.out.println("====== Welcome to Ecommerce Web Application ======");
 		System.out.println("1.Admin");
-		System.out.println("2.USer");
+		System.out.println("2.User");
 		System.out.println("0.Exit");
 		ch = sc.nextInt();
 
 		switch (ch) {
 		case 1:
-			System.out.println("Admin Screen");
+			admin.login();
 			break;
 		case 2:
 			System.out.println("User Screen ");
